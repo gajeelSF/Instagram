e0a3eb82899e0fbd94ecaeb44b5ed75863b19163
gajeelSF <SongYuda@SFanM.local>
2017-03-08 17:24:40 -0800

    basic setup

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Instagram"
+                configuration.clientKey = "0h289ffh208g3grhiueh082g2h3urh"  // set to nil assuming you have not set clientKey
+                configuration.server = "https://superultimateparsing.herokuapp.com/parse"
+            })
+        )
+        
         return true
     }
 
